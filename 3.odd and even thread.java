60c0e6f4f97581761064633342b342b6f5586490
ponica-jaya <53474212+ponica-jaya@users.noreply.github.com>
2020-05-20 12:30:13 +0530

    Update 3.odd and even thread.java

--- 3.odd and even thread.java
+++ 3.odd and even thread.java
@@ -1,3 +1,19 @@
+/* Q2.Write down a java program to print even and odd numbers series respectively
+from two threads: t1 and t2 synchronizing on a shared object
+Let t1 print message “ping — >” and t2 print message “,—-pong”.
+Take as command line arguments, the following inputs to the program:
+Sleep Interval for thread t1
+Write down a java program to print even and odd numbers series respectively from two
+threads: t1 and t2 synchronizing on a shared object
+Let t1 print message “ping — >” and t2 print message “,—-pong”.
+Take as command line arguments, the following inputs to the program:
+Sleep Interval for thread t1
+Sleep Interval for thread t2
+Message per cycle
+No of cycles
+ans:*/
+
+
 class OddThread extends Thread
 {
 int limit;
