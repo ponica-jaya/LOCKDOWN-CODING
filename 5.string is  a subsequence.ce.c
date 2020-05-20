52abe9d6f0a9e821f6c6ba469fd1a076a7566be3
ponica-jaya <53474212+ponica-jaya@users.noreply.github.com>
2020-05-20 12:27:55 +0530

    Update 5.string is  a subsequence.ce.c

--- 5.string is  a subsequence.ce.c
+++ 5.string is  a subsequence.ce.c
@@ -1,3 +1,15 @@
+/* Q.A user will input two strings, and we find if one of the strings is a sub sequence of the other. Program prints “yes” if either the first string is a sub sequence of the second string or the second string is a sub sequence of the first string.
+Assume that, the length of the first string is smaller than or equal to the length of the second string.
+An expected output of the program:
+Input the first string
+tree
+Input the second string
+Computer science is awesome
+YES
+Ans: */
+
+
+
 #include <stdio.h>
 #include <string.h>
 
